30906c88754c371c34fee03ed0c1cb316d232bf8
aaronchowapcs <aahchow@gmail.com>
2014-09-02 10:13:02 -0700

    added first name to index.html

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,52 @@
+//Aaron Chow
+//AP Computer Science Block 3
+//This is my original Landscape design
+
 void setup()
 {
-
+  size(425,425);
 }
 void draw()
 {
-  ellipse(50,50,50,50);
+  background(0, 166,255);
+  sun();
+  clouds();
+  hills();
+  road();
+  nightTime();
 }
-
+void clouds()
+{
+  fill(255, 255, 255);
+  ellipse(286,79,106,76);
+  ellipse(341,77,64,56);
+  ellipse(223,77,64,56);
+}
+void hills()
+{
+  fill(46, 199, 94);
+  noStroke();
+  ellipse(117,339,432,236);
+  ellipse(329,369,322,200);
+}
+void sun()
+{
+  fill(230, 220, 32);
+  ellipse(mouseX,mouseY,150,150);
+}
+void road()
+{
+  fill(0, 0, 0);
+  bezier(216,514,98,436,348,244,347,273);
+}
+void nightTime()
+{
+	if(mouseY>117 && mouseY>339)
+		{
+		background(0,0,0);
+        sun();
+  		clouds();
+  		hills();
+  		road();
+		}
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,15 +14,58 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
+//Aaron Chow
+//AP Computer Science Block 3
+//This is my original Landscape design
+
 public void setup()
 {
-
+  size(425,425);
 }
 public void draw()
 {
-  ellipse(50,50,50,50);
+  background(0, 166,255);
+  sun();
+  clouds();
+  hills();
+  road();
+  nightTime();
 }
-
+public void clouds()
+{
+  fill(255, 255, 255);
+  ellipse(286,79,106,76);
+  ellipse(341,77,64,56);
+  ellipse(223,77,64,56);
+}
+public void hills()
+{
+  fill(46, 199, 94);
+  noStroke();
+  ellipse(117,339,432,236);
+  ellipse(329,369,322,200);
+}
+public void sun()
+{
+  fill(230, 220, 32);
+  ellipse(mouseX,mouseY,150,150);
+}
+public void road()
+{
+  fill(0, 0, 0);
+  bezier(216,514,98,436,348,244,347,273);
+}
+public void nightTime()
+{
+	if(mouseY>117 && mouseY>339)
+		{
+		background(0,0,0);
+        sun();
+  		clouds();
+  		hills();
+  		road();
+		}
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
